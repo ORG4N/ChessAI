c17bb2a7fbf8614ef7e7dd754d05380cf972067f
datguycallum@gmail.com <datguycallum@gmail.com>
2023-03-03 18:07:09 +0000

    Play.html input validation

--- Code/static/js/play.js
+++ Code/static/js/play.js
@@ -4,11 +4,13 @@ function setStart(element){
     document.getElementById("rand").classList.remove("selected");
     document.getElementById("black").classList.remove("selected");
 
-    element.classList.add("selected")
+    element.classList.add("selected");
+    document.getElementById("side-input").value = element.id;
+
 }
 
 // Simple function to show dropdown
-function Dropdown(id){
+function dropdown(id){
     const dropdown_element = id + "-values";
 
     document.getElementById(dropdown_element).classList.toggle("show");
@@ -31,8 +33,10 @@ function setDiff(element){
     element.classList.add("selected");
 
     // Replace text to show selected VALUE
-    const text = document.getElementById("difficulty-selected");
-    text.innerText = value;
+    document.getElementById("difficulty-text").innerText = value;
+
+    // Set hidden INPUT FIELD value in form to value chosen
+    document.getElementById("difficulty-input").value = value;
 }
 
 // Simple function to set value of TIME and highlight button pressed
@@ -51,6 +55,64 @@ function setTime(element){
     element.classList.add("selected");
 
     // Replace text to show selected VALUE
-    const text = document.getElementById("time-selected");
-    text.innerText = element.innerText;
+    document.getElementById("time-text").innerText = element.innerText;
+
+    // Set hidden INPUT FIELD value in form to value chosen
+    document.getElementById("time-input").value = value;
+}
+
+// Function that validates whether STARTING SIDE, TIME, and DIFFICULTY have been selected.
+function validate(){
+
+    // Hide all VALIDATION text by default
+    document.getElementById("validate").classList.add("hidden");
+    document.getElementById("validate-side").classList.add("hidden");
+    document.getElementById("validate-rating").classList.add("hidden");
+    document.getElementById("validate-time").classList.add("hidden");
+
+
+    // Remove VALIDATION outline from all items
+    document.getElementById("side-box").classList.remove("invalid");
+    document.getElementById("rating-box").classList.remove("invalid");
+    document.getElementById("time-box").classList.remove("invalid");
+
+
+    const SIDE = document.getElementById("side-input").value;
+    const DIFF = document.getElementById("difficulty-input").value;
+    const TIME = document.getElementById("time-input").value;
+
+    if (!SIDE || !DIFF || !TIME){
+        document.getElementById("validate").classList.remove("hidden");
+
+        if (!SIDE){
+            document.getElementById("validate-side").classList.remove("hidden");
+            document.getElementById("side-box").classList.add("invalid");
+        }
+    
+        if (!DIFF){
+            document.getElementById("validate-rating").classList.remove("hidden");
+            document.getElementById("rating-box").classList.add("invalid");
+
+        }
+    
+        if (!TIME){
+            document.getElementById("validate-time").classList.remove("hidden");
+            document.getElementById("time-box").classList.add("invalid");
+        }
+
+        return "invalid";
+
+    }
+
+    return "valid"
+}
+
+// Function that will call VALIDATE() and then determine whether a match is created or user is directed to modify inputs.
+function submit(){
+    const valid = validate();
+
+    // If validation is correct then disable button
+    if (valid == "valid"){
+        document.getElementById("submit").toggleAttribute('disabled');
+    }
 }
