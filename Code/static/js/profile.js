c4708c88d20d04f1352c13af7bdafcb75695656d
datguycallum@gmail.com <datguycallum@gmail.com>
2023-04-25 21:54:34 +0100

    Profile linked to database

--- Code/static/js/profile.js
+++ Code/static/js/profile.js
@@ -1,21 +1,23 @@
 window.addEventListener('load', function () {
 
     const donut = document.getElementById('donut');
-    win = 40;
-    lose = 10;
+    won = 5;
+    lost = 4;
+    drawn = 1;
 
     data = {
         datasets: [{
-            data: [win,lose],
-            backgroundColor: ["blue", "red"],
+            data: [won,lost,drawn],
+            backgroundColor: ["blue", "red", "grey"],
             borderWidth:1,
             borderColor: "black"
         }],
     
         // These labels appear in the legend and in the tooltips when hovering different arcs
         labels: [
-            'Win',
-            'Lose',
+            'Won',
+            'Lost',
+            "Drawn"
         ]
     };
 
@@ -30,12 +32,46 @@ window.addEventListener('load', function () {
                     display: false
                 },
                 tooltip: {
-                    enabled: false
+                    enabled: true
                 }
             }
         }
     });
+
+    const sections = ["history", "badges", "friends"];
+
+    for (var i=0; i<sections.length; i++){
+
+        const section = sections[i];
+        const button_id = section + "-btn";
+        const button = document.getElementById(button_id);
+
+        button.addEventListener('click', function(){
+            // Change the styling of the nav button on the container
+            selected(button);
+        
+            // Display the currently selected content
+            content(button);
+        });
+    }
+
 })
+
+// Show selected section but hide others.
+function content(clicked){
+
+    // Hide all section
+    document.getElementById("history-section").classList.add("hidden");
+    document.getElementById("badges-section").classList.add("hidden");
+    document.getElementById("friends-section").classList.add("hidden");
+
+    // Create target to get element with section id
+    const target = clicked.id.split("-")[0] + "-section";
+    const target_id = document.getElementById(target);
+
+    // Remove hidden class from target we want to display
+    target_id.classList.remove("hidden");
+}
 
 // Selcted button has a unique style.
 function selected(clicked){
